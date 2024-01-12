61d923a162f7e4616283b7e5a098da87c0625eac
Fattesingh Rane <ranefattesingh@gmail.com>
2024-01-12 22:57:43 +0530

    fix: ripgrep installation

--- ubuntu.sh
+++ ubuntu.sh
@@ -217,8 +217,14 @@ install_minikube() {
 }
 
 install_ripgrep() {
-	curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
-	sudo dpkg -i ripgrep_13.0.0_amd64.deb
+    read -p "Do you wish to install ripgrep? [Y/n]" yn
+    case $yn in
+        [Yy] ) 	curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+				sudo dpkg -i ripgrep_13.0.0_amd64.deb
+				rm -rf ripgrep_13.0.0_amd64.deb;;
+    esac
+	
+
 }
 
 
